8f221c48b14a9feaf87847feffd79d97e1a957f4
Sakib Rezoan Reza <bsse0611@iit.du.ac.bd>
2017-09-29 04:49:40 +0600

    Change the welcome message for practice

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome To </br>
+                    Welcome</br>
                     Intelligent Career Portal
                 </div>
 
