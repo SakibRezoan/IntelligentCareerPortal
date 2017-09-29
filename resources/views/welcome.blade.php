a8907125b9e23f105e7ba93f83e8e84cee3cb4e6
Sakib Rezoan Reza <bsse0611@iit.du.ac.bd>
2017-09-29 20:09:16 +0600

    another test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,14 @@
                 <div class="title m-b-md">
                     Welcome</br>
                     Intelligent Career Portal
-                    <p>worked in new branch</p>
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
