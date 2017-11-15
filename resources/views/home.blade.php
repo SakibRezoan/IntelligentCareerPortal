ce78fd48459ebaad5a8f55b2ad02c0ced4669d81
Sakib Rezoan Reza <bsse0611@iit.du.ac.bd>
2017-11-15 14:57:46 +0600

    Job Seeker Side Bar added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,40 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0
-        col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
-
+        <div class="col-sm-4 col-md-3 sidebar">
+            <div class="mini-submenu">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </div>
+            <div class="list-group">
+        <span href="#" class="list-group-item active">
+            Dashboard
+            <span class="pull-right" id="slide-submenu">
+                <i class="fa fa-times"></i>
+            </span>
+        </span>
+                <a href="#" class="list-group-item">
+                    <i class="fa fa-comment-o"></i> General Information
+                </a>
+                <a href="#" class="list-group-item">
+                    <i class="fa fa-search"></i> Education
+                </a>
+                <a href="#" class="list-group-item">
+                    <i class="fa fa-user"></i> Work Experience
+                </a>
+                <a href="#" class="list-group-item">
+                    <i class="fa fa-user"></i> Job Preference
+                </a>
+                <a href="#" class="list-group-item">
+                    <i class="fa fa-user"></i> Teams
+                </a>
+                <a href="#" class="list-group-item">
+                    <i class="fa fa-folder-open-o"></i> Recommended Jobs <span class="badge">14</span>
+                </a>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-8 col-md-7 toppad" >
             <div class="panel panel-info">
                 <div class="panel-heading">
                     <h3 class="panel-title">General Information</h3>
